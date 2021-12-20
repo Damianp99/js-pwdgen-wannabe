4fe2db109f68fbc4a0cd98c5998fafe5f2ff9c24
Damianp99 <damianperez200199@gmail.com>
2021-12-20 17:05:27 +0100

    nome e cognome

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//inserisci nome
+
+var nome = prompt('inserisci il tuo nome');
+console.log(nome);
+
+//inserisci cognome
+
+var cognome = prompt('inserisci il tuo cognome');
+console.log(cognome)
+
