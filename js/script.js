aabca58d6e0695ce31b5eb4b542681d51c293d4c
Damianp99 <damianperez200199@gmail.com>
2021-12-20 17:19:07 +0100

    added color + printing on the page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 //inserisci nome
 
-var nome = prompt('inserisci il tuo nome');
+const nome = prompt('Inserisci il tuo nome');
 console.log(nome);
 
 //inserisci cognome
 
-var cognome = prompt('inserisci il tuo cognome');
+const cognome = prompt('Inserisci il tuo cognome');
 console.log(cognome)
 
+//colore preferito
+
+const colorePreferito = prompt('Inserisci il tuo colore preferito');
+console.log(colorePreferito)
+
+//stampa l'elemento nella pagina
+document.getElementById('password').innerHTML = 'La tua password è :' + nome + cognome + colorePreferito + '21'
